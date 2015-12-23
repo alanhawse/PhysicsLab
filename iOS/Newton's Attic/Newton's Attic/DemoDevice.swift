48b6f22f242808d78d8731a7e2b4faf6100fe089
Alan Hawse <arh@Alans-MacBook-Pro.local>
2015-12-23 13:33:35 -0500

    first update for demo mode

---
+++ iOS/Newton's Attic/Newton's Attic/DemoDevice.swift
@@ -0,0 +1,9 @@
+//
+//  DemoDevice.swift
+//  Newton's Attic
+//
+//  Created by Alan Hawse on 12/22/15.
+//  Copyright © 2015 Elkhorn Creek Engineering. All rights reserved.
+//
+
+import Foundation
