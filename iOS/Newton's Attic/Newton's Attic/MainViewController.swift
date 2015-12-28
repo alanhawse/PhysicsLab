11425c48fbbbdc5a48d5986c9dd0a4fbb313f247
Alan Hawse <arh@cypress.com>
2015-12-28 05:28:53 -0500

    put in placeholder for modal popover

--- iOS/Newton's Attic/Newton's Attic/MainViewController.swift
+++ iOS/Newton's Attic/Newton's Attic/MainViewController.swift
@@ -46,6 +46,10 @@ class MainViewController: UITableViewController {
         {
             if let tvc = sender as? UITableViewCell {
                 tbc.bleD = tagToId[tvc.tag]
+                // can you do a popOver right here?
+                if tbc.bleD?.demoDevice != nil {
+                    print("This is a demo device")
+                }
             }
         }
         NSNotificationCenter.defaultCenter().removeObserver(self)
